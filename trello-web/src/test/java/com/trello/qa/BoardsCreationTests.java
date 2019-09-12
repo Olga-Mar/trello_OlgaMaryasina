d79ded881dd496bb9196255b466b6eaa989a607f
Olga-Mar <marina.yasina@gmail.com>
2019-09-12 12:51:51 +0300

    BoardsCreation,Wait.until

--- trello-web/src/test/java/com/trello/qa/BoardsCreationTests.java
+++ trello-web/src/test/java/com/trello/qa/BoardsCreationTests.java
@@ -2,40 +2,40 @@ package com.trello.qa;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class BoardsCreationTests extends TestBase {
+    @BeforeClass
+    public void ensurePreconditionsLogin(){
+        if(!isUserLoggedIn()){
+            login("marina.yasina@gmail.com", "SofiaAmalia2016");
+        }
+    }
+
 
     @Test
     public void testBoardCreationFromPlusButtonOnHeader(){
+        int beforCreation=getPersnalBoardsCount();
         clickOnPlusButtonOnHeader();
-        selectCreateTeamFromDropDown();
-        AddTitleOnCreationBord("First pancake");
-        ClickOnCreationBoardButton();
-        //Assert
-        Assert.assertTrue(isUserLoggedIn());
+        selectCreateBoardFromDropDown();
+        fillBoardCreationForm("First pankace","no");
+        confirmBoardCreation();
+        returnToHomePage();
+
+        int afterCreation=getPersnalBoardsCount();
+      Assert.assertEquals(afterCreation, beforCreation+1);
+       // Assert.assertTrue(isUserLoggedIn());
 
     }
 
-    private void ClickOnCreationBoardButton() {
-        click(By.cssSelector("[type='button']"));
-
-
-    }
-
-    private void AddTitleOnCreationBord(String boardName) {
+  public void fillBoardCreationForm(String boardName) {
         type(By.cssSelector("[data-test-id='header-create-board-title-input']"),boardName);
       //  click(By.cssSelector("[class='W6rMLOx8U0MrPx']")); //("[class='_1vk4y48RR5OmqE']"));
       //  click(By.cssSelector("[class='_1Lkx3EjS3wCrs7']"));
       //  click(By.cssSelector("[name='public']"));
 
-    }
 
-    private void selectCreateTeamFromDropDown() {
-        click(By.cssSelector("[data-test-id='header-create-board-button']"));
-    }
 
-    private void clickOnPlusButtonOnHeader() {
-        click(By.cssSelector("[data-test-id='header-create-menu-button']"));
     }
 }
