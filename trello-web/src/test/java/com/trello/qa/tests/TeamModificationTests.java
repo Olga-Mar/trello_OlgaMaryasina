3bbb449ab077475204cdce169b168c2b394dcefe
Olga-Mar <marina.yasina@gmail.com>
2019-09-28 11:33:51 +0300

    ApplicationManager and Helpers

---
+++ trello-web/src/test/java/com/trello/qa/tests/TeamModificationTests.java
@@ -0,0 +1,27 @@
+package com.trello.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TeamModificationTests extends TestBase {
+    @BeforeMethod
+    public void precodition(){
+        if(!app.getTeamHelper().isTeamsPresent()){
+            app.getTeamHelper().createTeam();
+        }
+    }
+
+    @Test
+    public void testRenameTeam() throws InterruptedException {
+        app.getTeamHelper().clickOnFirstTeam();
+        app.getTeamHelper().openSettings();
+        app.getTeamHelper().initEditTeamProfil();
+        String teamName = "hh";
+        app.getTeamHelper().changeTeamProfile(teamName, "hha");
+        app.getTeamHelper().confirmEditTeam();
+        String createdTeamName = app.getTeamHelper().getTeamNameFromTeamPage();
+        Assert.assertEquals(createdTeamName, teamName);
+        app.getTeamHelper().returnToHomePage();
+    }
+}
